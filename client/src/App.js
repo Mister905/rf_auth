7d2eb33b819d496a9e266051c55c39170eb4bf9d
James McCarthy <james.joseph.mccarthy.905@gmail.com>
2021-11-06 18:22:19 -0400

    Hooks refactor for App and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import { compose } from "redux";
-import { withRouter } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 // Components
 import Landing from "./components/landing/Landing";
 import Modal from "./components/modal/Modal";
@@ -15,60 +14,43 @@ import Update_Product from "./components/update_product/Update_Product";
 import View_Product from "./components/view_product/View_Product";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import PublicRoute from "./components/routing/PublicRoute";
-
 // Actions
 import { load_active_user } from "./actions/auth";
 
-class App extends Component {
-  componentDidMount = () => {
-    this.props.load_active_user();
-  };
 
-  componentDidUpdate = (prevProps) => {
-    if (this.props.auth.is_authenticated !== prevProps.auth.is_authenticated) {
-      if (this.props.auth.is_authenticated) this.props.load_active_user();
-    }
-  };
+function App() {
 
-  render() {
-    const { display_modal } = this.props.modal;
+  const dispatch = useDispatch();
 
-    return (
-      <div className="App">
-        <Header />
-        {display_modal && <Modal />}
-        <Switch>
-          <PublicRoute exact path="/" component={Landing} />
-          <PublicRoute exact path="/login" component={Login} />
-          <PublicRoute exact path="/register" component={Register} />
-          <PrivateRoute exact path="/products" component={Products} />
-          <PrivateRoute
-            exact
-            path="/create_product"
-            component={Create_Product}
-          />
-          <PrivateRoute
-            exact
-            path="/view_product/:id"
-            component={View_Product}
-          />
-          <PrivateRoute
-            exact
-            path="/update_product/:id"
-            component={Update_Product}
-          />
-        </Switch>
-      </div>
-    );
-  }
+  const is_authenticated = useSelector(state => state.auth.is_authenticated);
+
+  const display_modal = useSelector(state => state.modal.display_modal);
+
+  useEffect(() => {
+
+    dispatch(load_active_user());
+
+  }, [is_authenticated]);
+
+  return (
+    <div className="App">
+      <Header />
+      {display_modal && <Modal />}
+      <Switch>
+        <PublicRoute exact path="/" component={Landing} />
+        <PublicRoute exact path="/login" component={Login} />
+        <PublicRoute exact path="/register" component={Register} />
+        <PrivateRoute exact path="/products" component={Products} />
+        <PrivateRoute exact path="/create_product" component={Create_Product} />
+        <PrivateRoute exact path="/view_product/:id" component={View_Product} />
+        <PrivateRoute
+          exact
+          path="/update_product/:id"
+          component={Update_Product}
+        />
+      </Switch>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-  modal: state.modal,
-});
-
-export default compose(
-  connect(mapStateToProps, { load_active_user }),
-  withRouter
-)(App);
+export default App;
